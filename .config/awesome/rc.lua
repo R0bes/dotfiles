01a6ce18003e69998613a249d4c87b9597f11f00
Robs <rob.schwe@gmail.com>
2023-01-11 13:34:39 +0100

    dmenu

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -311,7 +311,7 @@ globalkeys = gears.table.join(
               {description = "restore minimized", group = "client"}),
 
     -- Prompt
-    awful.key({ modkey },            "r",     function () awful.screen.focused().mypromptbox:run() end,
+    awful.key({ modkey },            "r",     function ()  awful.util.spawn("dmenu_run") end,
               {description = "run prompt", group = "launcher"}),
 
     awful.key({ modkey }, "x",
@@ -562,3 +562,9 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+
+awful.spawn.with_shell("picom")
+awful.spawn.with_shell("nitrogen --restore")
+
+
---
+++ .config/nvim/init.vim
@@ -0,0 +1,4 @@
+
+colo molokai 
+
+syntax on
